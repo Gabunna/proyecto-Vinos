c1e2b16c84d7314ef106b1e47aad655d9c2efb11
camilo <camilopezzoni@gmail.com>
2023-10-15 01:33:08 -0300

    catalogo con filtro

---
+++ js/data.js
@@ -0,0 +1,75 @@
+const listaDeArticulosJSON = [
+    {
+        "_id": 1,
+        "nombre": "vino blanco reserva1",
+        "foto": "vino-bco1.jpg",
+        "tipo": "blanco",
+        "descripcion": "descripción del vino",
+        "precio": 1000
+    },
+    {
+        "_id": 2,
+        "nombre": "vino blanco reserva2",
+        "foto": "vino-bco2.jpg",
+        "tipo": "blanco",
+        "descripcion": "descripción del vino",
+        "precio": 1000
+    },
+    {
+        "_id": 3,
+        "nombre": "vino blanco reserva3",
+        "foto": "vino-bco3.jpg",
+        "tipo": "blanco",
+        "descripcion": "descripción del vino",
+        "precio": 1000
+    },
+    {
+        "_id": 4,
+        "nombre": "vino blanco reserva4",
+        "foto": "vino-bco4.jpg",
+        "tipo": "blanco",
+        "descripcion": "descripción del vino",
+        "precio": 1000
+    },
+    {
+        "_id": 5,
+        "nombre": "vino tinto reserva1",
+        "foto": "vino-tinto1.jpg",
+        "tipo": "tinto",
+        "descripcion": "descripción del vino",
+        "precio": 1000
+    },
+    {
+        "_id": 6,
+        "nombre": "vino tinto reserva2",
+        "foto": "vino-tinto2.jpg",
+        "tipo": "tinto",
+        "descripcion": "descripción del vino",
+        "precio": 1000
+    },
+    {
+        "_id": 7,
+        "nombre": "vino tinto reserva3",
+        "foto": "vino-tinto3.jpg",
+        "tipo": "tinto",
+        "descripcion": "descripción del vino",
+        "precio": 1000
+    },
+    {
+        "_id": 8,
+        "nombre": "vino tinto reserva4",
+        "foto": "vino-tinto4.jpg",
+        "tipo": "tinto",
+        "descripcion": "descripción del vino",
+        "precio": 1000
+    },
+    {
+        "_id": 9,
+        "nombre": "vino espumante1",
+        "foto": "espumante1.jpg",
+        "tipo": "espumante",
+        "descripcion": "descripción espumante",
+        "precio": 1000
+    }
+    
+];
